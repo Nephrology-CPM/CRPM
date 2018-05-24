6399486848c06a189e1dfe3c14ce54359402d523
Daniel Montemayor <dr.daniel.montemayor@gmail.com>
2018-05-24 13:13:17 -0500

    setup integration

---
+++ .circleci/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -eu
+
+WORKSPACE=$(pwd)
+
+# Common definitions (downloaded from github)
+source .circleci/common.sh
+
+# Set path
+echo "export PATH=$WORKSPACE/miniconda/bin:$PATH" >> $BASH_ENV
+source $BASH_ENV
+
+if [[ ! -d $WORKSPACE/miniconda ]]; then
+    # setup conda and dependencies if not loaded from cache
+    mkdir -p $WORKSPACE
+
+    # step 1: download and install miniconda
+    if [[ $OSTYPE == darwin* ]]; then
+        tag="MacOSX"
+    elif [[ $OSTYPE == linux* ]]; then
+        tag="Linux"
+    else
+        echo "Unsupported OS: $OSTYPE"
+        exit 1
+    fi
+    curl -L -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VER-$tag-x86_64.sh
+    bash miniconda.sh -b -p $WORKSPACE/miniconda
+
+    # step 2: setup channels
+    conda config --system --add channels defaults
+    conda config --system --add channels conda-forge
+    #conda config --system --add channels bioconda
+
+    # step 3: install bioconda-utils and test requirements
+    conda install -y --file requirements.txt --file test-requirements.txt
+
+    # step 4: cleanup
+    conda clean -y --all
+
+    # Add local channel as highest priority
+    conda index $WORKSPACE/miniconda/conda-bld/linux-64 $WORKSPACE/miniconda/conda-bld/osx-64 $WORKSPACE/miniconda/conda-bld/noarch
+    conda config --system --add channels file://$WORKSPACE/miniconda/conda-bld
+fi
+
+conda config --get
+
+ls $WORKSPACE/miniconda/conda-bld
+ls $WORKSPACE/miniconda/conda-bld/noarch
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CRPM',
+    name='crpm',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Data tools for the Center for Renal Precision Medicine at the University of Texas Health San Antonio.',
     long_description=open('README.md').read(),
-    install_requires=[''],
+    #install_requires=[''],
     url='https://github.com/dmontemayor/CRPM',
     author='Daniel Montemayor',
     author_email='montemayord2@uthscsa.edu'
+    classifiers=[
+    "Development Status :: 1 - Planning",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+
 )
